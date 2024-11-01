a1f8bff63d197e97d036b79a7091199fd78d92b0
VaibhavShukla4 <96537580+VaibhavShukla4@users.noreply.github.com>
2024-11-01 22:53:49 +0530

    add

--- app/users/index.tsx
+++ app/users/index.tsx
@@ -4,7 +4,12 @@ import React from 'react';
 import Layout from './layout';
 
 const Users = () => {
-  return <Layout></Layout>;
+  return (
+    <Layout>
+      <div>Welcome to the Users Page!</div>
+      {/* You can add more content here as needed */}
+    </Layout>
+  );
 };
 
 export default Users;
