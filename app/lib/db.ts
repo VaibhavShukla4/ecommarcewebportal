cfd82b401b1ac058a4ac5ac48a194ba521d7558f
VaibhavShukla4 <96537580+VaibhavShukla4@users.noreply.github.com>
2024-11-01 23:05:01 +0530

    agin update

--- app/lib/db.ts
+++ app/lib/db.ts
@@ -1,9 +1,7 @@
-/** @format */
-
 const { DB_USERNAME, DB_PASSWORD, DB_CLUSTER } = process.env;
 
 if (!DB_USERNAME || !DB_PASSWORD || !DB_CLUSTER) {
   throw new Error("Please define the DB_USERNAME, DB_PASSWORD, and DB_CLUSTER environment variables.");
 }
 
-export const MONGODB_URI = `mongodb+srv://${DB_USERNAME}:${DB_PASSWORD}@${DB_CLUSTER}.mongodb.net/yourDatabaseName`;
+export const MONGODB_URI = `mongodb+srv://${DB_USERNAME}:${DB_PASSWORD}@${DB_CLUSTER}.mongodb.net/yourDatabaseName?retryWrites=true&w=majority`;
