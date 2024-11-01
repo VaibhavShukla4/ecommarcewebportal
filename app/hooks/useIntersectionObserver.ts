61c6075a83eca574325e19d8c6fcd2a388c90c5a
VaibhavShukla4 <96537580+VaibhavShukla4@users.noreply.github.com>
2024-11-01 22:25:47 +0530

    update hook

--- app/hooks/useIntersectionObserver.ts
+++ app/hooks/useIntersectionObserver.ts
@@ -1,11 +1,19 @@
-/** @format */
-
 import { useEffect, useRef } from 'react';
 
-const useIntersectionObserver = (callback, options) => {
-  const ref = useRef(null);
+// Define types for the callback and options parameters
+const useIntersectionObserver = (
+  callback: (entries: IntersectionObserverEntry[]) => void,
+  options?: IntersectionObserverInit
+) => {
+  const ref = useRef<Element | null>(null);
 
   useEffect(() => {
+    // Check if IntersectionObserver is supported
+    if (!('IntersectionObserver' in window)) {
+      console.warn('IntersectionObserver is not supported in this browser.');
+      return;
+    }
+
     const observer = new IntersectionObserver((entries) => {
       callback(entries);
     }, options);
