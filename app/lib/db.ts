abb0c49bff628aa3d69ddde9f2b3c836e6074d23
VaibhavShukla4 <96537580+VaibhavShukla4@users.noreply.github.com>
2024-11-02 09:29:51 +0530

    add env

--- app/lib/db.ts
+++ app/lib/db.ts
@@ -4,5 +4,4 @@ if (!DB_USERNAME || !DB_PASSWORD || !DB_CLUSTER) {
   throw new Error("Please define the DB_USERNAME, DB_PASSWORD, and DB_CLUSTER environment variables.");
 }
 
-export  const MONGODB_URI = `mongodb+srv://${DB_USERNAME}:${DB_PASSWORD}@${DB_CLUSTER}.mongodb.net/yourDatabaseName?retryWrites=true&w=majority`;
-
+export const MONGODB_URI = `mongodb+srv://${DB_USERNAME}:${DB_PASSWORD}@${DB_CLUSTER}.mongodb.net/yourDatabaseName?retryWrites=true&w=majority`;
